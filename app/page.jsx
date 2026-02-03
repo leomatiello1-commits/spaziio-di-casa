ace5219383639186667d0fe0bf60a5e49ad84c7b
leomatiello1-commits <leomatiello1@gmail.com>
2026-02-03 01:50:14 -0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "serif" }}>
+      <h1>Spazio di Casa</h1>
+      <p>Ambiente interno de organização de processos e documentos.</p>
+      <p>Acesso restrito por link privado.</p>
+    </div>
+  );
+}
